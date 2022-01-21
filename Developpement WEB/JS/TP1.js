bf00113bbeac283907fc5a7aa73d19bf69c54eb6
kobbi <wassef.kobbi@gmail.com>
2022-01-21 13:55:40 +0100

    Retour aprés la resolution du problémé du github

---
+++ Developpement WEB/JS/TP1.js
@@ -0,0 +1,6 @@
+let nomber1 = 20;
+let nomber2 = 56;
+const nomber = nomber1 + nomber2;
+alert("Bienvenue sur la page de formulaire");
+console.log(nomber1, nomber2, nomber);
+console.log(`Le résultat est un ${nomber}`);
---
+++ Developpement WEB/JS/TP2.js
@@ -0,0 +1,5 @@
+const Distance = prompt('La distance parcours(m)');
+const Temps = prompt('Le temps(sec)');
+const Vitesse = Distance/Temps;
+alert(`La vitess est Vitesse= ${V} m/s`); 
+console.log(`La vitess est Vitesse= ${V} m/s`);
